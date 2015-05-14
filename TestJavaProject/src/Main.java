8e4d1ad519067295c63d57ac6f7d09de0b86784a
mongkonchai <mongkonchai@NR-0000021.locus.co.th>
2015-05-15 01:02:19 +0700

    sd

--- TestJavaProject/src/Main.java
+++ TestJavaProject/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
+		System.out.println("Hello4");
 	}
 
 }
